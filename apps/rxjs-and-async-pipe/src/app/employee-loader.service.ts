fd3457a5aab3f645440b7b5a2cf987867e00ae21
Bill Odom <bill@billodom.com>
2025-05-13 16:17:11 +0000

    Use external API; add logging with tap()

--- apps/rxjs-and-async-pipe/src/app/employee-loader.service.ts
+++ apps/rxjs-and-async-pipe/src/app/employee-loader.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
-const apiUrl = '/api';
+// const apiUrl = '/api';
 
 // Or connect to the hosted demo API (works from StackBlitz):
-// const apiUrl = 'https://api.angularbootcamp.com';
+const apiUrl = 'https://api.angularbootcamp.com';
 
 export interface Employee {
   id: number;
@@ -20,8 +20,10 @@ export class EmployeeLoaderService {
   private http = inject(HttpClient);
 
   loadEmployees(): Observable<Employee[]> {
-    return this.http
-      .get<Employee[]>(apiUrl + '/employees')
-      .pipe(map(employees => employees.slice(0, 5)));
+    return this.http.get<Employee[]>(apiUrl + '/employees').pipe(
+      tap(data => console.log(data)),
+      map(employees => employees.slice(0, 5)),
+      tap(data => console.log(data))
+    );
   }
 }
